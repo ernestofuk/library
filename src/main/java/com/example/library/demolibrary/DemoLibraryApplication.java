a060b71fcca8b9949f2de08741c16edeff344339
Ernesto Fuk <ernestoa.fuk@gmail.com>
2020-03-14 06:34:21 -0500

    Initial Commit

---
+++ src/main/java/com/example/library/demolibrary/DemoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.library.demolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/library/demolibrary/DemoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library.demolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
