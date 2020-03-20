3185f79c782192e6e4994572db82de6de02ce087
Ernesto Fuk <ernestoa.fuk@gmail.com>
2020-03-19 23:58:52 -0500

    a

--- src/main/java/com/example/library/demolibrary/controller/BookController.java
+++ src/main/java/com/example/library/demolibrary/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,6 +50,6 @@ public class BookController {
   @GetMapping(value = "/book-search")
   public ResponseEntity getBook(@RequestParam Map<String, String> allParams) {
     List<Book> books = bookService.getBook(allParams);
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok(books);
   }
 }
